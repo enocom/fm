24918cab67863f25995727deb8c2662950a66c54
Eno Compton <eno4@ecom.com>
2016-12-29 10:07:17 -0700

    Remove irrelevant code from doer

--- example/doer.go
+++ example/doer.go
@@ -14,14 +14,3 @@ type Delegater struct {
 func (d *Delegater) DoSomething(task string) (int, error) {
 	return d.Delegate.DoIt(task, false)
 }
-
-// RealDoer get stuff done for real
-type RealDoer struct {
-}
-
-// DoIt does all the hard work
-func (r *RealDoer) DoIt(task string, repeat bool) (int, error) {
-	return 0, nil
-}
-
-const someConst = 1
