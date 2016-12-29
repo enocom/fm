801ec2795baad4d74871dbf3c11a0b92809f0500
Eno Compton <eno4@ecom.com>
2016-12-29 10:54:49 -0700

    Add documentation to Doer and Delegator

--- example/doer.go
+++ example/doer.go
@@ -1,14 +1,16 @@
 package example
 
-// Doer does things, sometimes repeatedly
+// Doer does things, sometimes graciously
 type Doer interface {
 	DoIt(task string, graciously bool) (int, error)
 }
 
+// Delegater employs a Doer to complete tasks
 type Delegater struct {
 	Delegate Doer
 }
 
+// DoSomething passes the work to Doer
 func (d *Delegater) DoSomething(task string) (int, error) {
 	return d.Delegate.DoIt(task, false)
 }
