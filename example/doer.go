8311d4ddb7658469e77b352988a6e53b8287cfbb
Eno Compton <eno4@ecom.com>
2016-12-29 11:39:19 -0700

    Add tests for use of Repeater interface

--- example/doer.go
+++ example/doer.go
@@ -8,9 +8,15 @@ type Doer interface {
 // Delegater employs a Doer to complete tasks
 type Delegator struct {
 	Delegate Doer
+	Repeater
 }
 
 // DoSomething passes the work to Doer
 func (d *Delegator) DoSomething(task string) (int, error) {
 	return d.Delegate.DoIt(task, false)
+}
+
+// DoSomethingAgain ensures a task is repeated with a rationale
+func (d *Delegator) DoSomethingAgain(task, rationale string) (int, error) {
+	return d.Repeat(task, rationale)
 }
--- example/doer_test.go
+++ example/doer_test.go
@@ -65,3 +65,62 @@ func TestDelegatorReturnsDoerResult(t *testing.T) {
 		t.Errorf("wanted: %v, but got %v", wantRet1, gotRet1)
 	}
 }
+
+func TestDelegatorCallsRepeater(t *testing.T) {
+	r := &FakeRepeater{}
+	d := &example.Delegator{Repeater: r}
+
+	d.DoSomethingAgain("laundry", "still not done")
+
+	want := true
+	got := r.Repeat_Called
+
+	if want != got {
+		t.Errorf("wanted %v, but got %v", want, got)
+	}
+}
+
+func TestDelegatorCallsRepeaterWithArgs(t *testing.T) {
+	r := &FakeRepeater{}
+	d := &example.Delegator{Repeater: r}
+
+	d.DoSomethingAgain("walk the dog", "he still won't sit still")
+
+	wantArg0 := "walk the dog"
+	gotArg0 := r.Repeat_Input.Arg0
+
+	if wantArg0 != gotArg0 {
+		t.Errorf("wanted %v, but got %v", wantArg0, gotArg0)
+	}
+
+	wantArg1 := "he still won't sit still"
+	gotArg1 := r.Repeat_Input.Arg1
+
+	if wantArg1 != gotArg1 {
+		t.Errorf("wanted %v, but got %v", wantArg1, gotArg1)
+	}
+}
+
+func TestDelegatorReturnsRepeaterResult(t *testing.T) {
+	r := &FakeRepeater{}
+	r.Repeat_Output.Ret0 = 42
+	expectedErr := errors.New("cat refuses")
+	r.Repeat_Output.Ret1 = expectedErr
+	d := &example.Delegator{Repeater: r}
+
+	num, err := d.DoSomethingAgain("walk the cat", "it's trying to kill me")
+
+	wantRet0 := 42
+	gotRet0 := num
+
+	if wantRet0 != gotRet0 {
+		t.Errorf("wanted %v, but got %v", wantRet0, gotRet0)
+	}
+
+	wantRet1 := expectedErr
+	gotRet1 := err
+
+	if wantRet1 != gotRet1 {
+		t.Errorf("wanted %v, but got %v", wantRet1, gotRet1)
+	}
+}
