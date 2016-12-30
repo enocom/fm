228c843ba6ad841f696ad528f5ddd674570eefbf
Eno Compton <eno4@ecom.com>
2016-12-29 22:42:07 -0700

    Add version flag

--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+
 	fm "github.com/enocom/fm/lib"
 )
 
+const Version = "1.0.0"
+
 func main() {
+	printVersion := flag.Bool("version", false, "Print version and exit")
+	flag.Parse()
+
+	if *printVersion {
+		fmt.Printf("fm version %s\n", Version)
+		return
+	}
+
 	conv := &fm.SpyStructConverter{}
 	impl := &fm.SpyFuncImplementer{}
 	g := &fm.SpyGenerator{Conv: conv, Impl: impl}
