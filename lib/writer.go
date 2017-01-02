ad9aa00b60b2c8f50a5ba7b3e021bd1183227e14
Eno Compton <eno4@ecom.com>
2017-01-01 22:26:07 -0700

    Add missing documentation to exported types

--- lib/writer.go
+++ lib/writer.go
@@ -7,12 +7,15 @@ import (
 	"os"
 )
 
+// ASTWriter writes the ast.File to the provided filename
 type ASTWriter interface {
 	Write(file *ast.File, filename string) error
 }
 
+// DiskASTWriter saves an AST to disk
 type DiskASTWriter struct{}
 
+// Write outputs the ast.File to a file on disk specified by filename
 func (d *DiskASTWriter) Write(file *ast.File, filename string) error {
 	spyFile, err := os.Create(filename)
 	if err != nil {
