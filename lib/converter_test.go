036080221477ff7cd909d0b5091a319476ad9d67
Eno Compton <eno4@ecom.com>
2017-01-08 23:02:51 -0700

    Add converter test confirming "_Called" field

--- lib/converter_test.go
+++ lib/converter_test.go
@@ -11,9 +11,7 @@ func TestConvertBuildsAddsSpyToTypeSpecName(t *testing.T) {
 	converter := &fm.SpyStructConverter{}
 
 	typeSpec := converter.Convert(
-		&ast.TypeSpec{
-			Name: ast.NewIdent("Tester"),
-		},
+		&ast.TypeSpec{Name: ast.NewIdent("Tester")},
 		&ast.InterfaceType{
 			Methods: &ast.FieldList{List: make([]*ast.Field, 0)},
 		},
@@ -24,5 +22,45 @@ func TestConvertBuildsAddsSpyToTypeSpecName(t *testing.T) {
 
 	if want != got {
 		t.Errorf("want %v, got %v", want, got)
+	}
+}
+
+func TestConvertAddsRecordOfFunctionCallAsField(t *testing.T) {
+	converter := &fm.SpyStructConverter{}
+
+	typeSpec := converter.Convert(
+		&ast.TypeSpec{Name: ast.NewIdent("Tester")},
+		&ast.InterfaceType{
+			Methods: &ast.FieldList{
+				List: []*ast.Field{
+					&ast.Field{
+						Names: []*ast.Ident{ast.NewIdent("Test")},
+						Type: &ast.FuncType{
+							Params:  &ast.FieldList{},
+							Results: &ast.FieldList{},
+						},
+					},
+				},
+			},
+		},
+	)
+
+	structType, ok := typeSpec.Type.(*ast.StructType)
+	if !ok {
+		t.Fatal("expected typeSpec to be of type StructType")
+	}
+
+	want := 1
+	got := len(structType.Fields.List)
+	if want != got {
+		t.Errorf("want %v, got %v", want, got)
+	}
+
+	calledField := structType.Fields.List[0]
+
+	wantName := "Test_Called"
+	gotName := calledField.Names[0].Name
+	if wantName != gotName {
+		t.Errorf("want %v, got %v", wantName, gotName)
 	}
 }
